1d58f6f618654f00d13ce982011ae006a53d6285
Mosab albishi <90303281+musabalbishi@users.noreply.github.com>
2023-06-10 05:33:34 +0300

    commit

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -12,41 +12,56 @@ import {
   CardBody,
   CardFooter,
 } from "@chakra-ui/react";
-
+import { useState, useEffect } from "react";
 // import { useState, useEffect } from "react";
 const Products = () => {
-  // const API_URL = "https://fakestoreapi.com/products";
+  // const [search, setSearch] = useState("");
+  const [item, setItem] = useState([]);
 
+  const getData = async () => {
+    const res = await fetch("https://fakestoreapi.com/products").then((res) =>
+      res.json()
+    );
+    setItem(res);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <Box as="section" p={"3rem"}>
       <Text>PRODUCTS</Text>
+
       <Flex gap={"2"} p={"3"} wrap={"wrap"} justifyContent={"start"}>
-        <Card maxW="sm">
-          <CardBody>
-            <Image
-              src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-              alt="Green double couch with wooden legs"
-              borderRadius="lg"
-            />
-            <Stack mt="6" spacing="3">
-              <Heading size="md">Living room Sofa</Heading>
-              <Text color="blue.600" fontSize="2xl">
-                $450
-              </Text>
-            </Stack>
-          </CardBody>
-          <Divider />
-          <CardFooter>
-            <ButtonGroup spacing="2">
-              <Button variant="solid" colorScheme="blue">
-                Buy now
-              </Button>
-              <Button variant="ghost" colorScheme="blue">
-                Add to cart
-              </Button>
-            </ButtonGroup>
-          </CardFooter>
-        </Card>
+        {item.map((data) => (
+          <Card maxW="sm" w={"20rem"} key={data.id}>
+            <CardBody>
+              <Image
+                h={"10rem"}
+                w={"10rem"}
+                src={data.image}
+                alt="Green double couch with wooden legs"
+                borderRadius="lg"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">{data.title}</Heading>
+                <Text color="blue.600" fontSize="2xl">
+                  {data.price + "$"}
+                </Text>
+              </Stack>
+            </CardBody>
+            <Divider />
+            <CardFooter>
+              <ButtonGroup spacing="2">
+                <Button variant="solid" colorScheme="blue">
+                  Buy now
+                </Button>
+                <Button variant="ghost" colorScheme="blue">
+                  Add to cart
+                </Button>
+              </ButtonGroup>
+            </CardFooter>
+          </Card>
+        ))}
       </Flex>
     </Box>
   );
